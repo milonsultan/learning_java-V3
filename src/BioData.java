41661fb367c1f5986b1bb83c8b5e34dddfbfc34f
milonsultan <milon_sultan@hotmail.com>
2023-09-09 16:54:55 +0200

    Initial commit

---
+++ src/BioData.java
@@ -0,0 +1,8 @@
+public class BioData {
+
+    public static void  main(String[] args) {
+
+        System.out.println("This is Nafiz");
+        System.out.println("I am new java learner");
+    }
+}
---
+++ src/Product.java
@@ -0,0 +1,28 @@
+public class Product {
+
+    public static void main(String[] args) {
+
+        String id = "101";
+        String title = "Iphone 15";
+        String price = "1895 Euro";
+        String description = "Perfect product with best image quality";
+        String category = "Phone";
+
+        System.out.println("Product Id: "+id);
+        System.out.println("Name: " +title);
+        System.out.println("Price: "+ price);
+        System.out.println("Description: "+description);
+        System.out.println("Category: "+category);
+
+
+        // single line comments
+
+        /*Escape Sequence learning- multi line comments
+
+         */
+        System.out.println("");
+
+        System.out.println("Product Id: "+id + '\n' + "Name: " +title + '\n' +"Price: "+ price + '\n' + "Description: "+description + '\n' + "Category: "+category);
+
+    }
+}
