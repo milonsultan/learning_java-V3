b44c83bc4ed6bfb07069fc12cdf6990336c7df50
milonsultan <milon_sultan@hotmail.com>
2023-09-12 23:52:10 +0200

    Added comment

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,13 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+
+        int num1, num2, result;
+
+        num1 = 10;
+        num2 =20;
+        result = num1 + num2;
+        System.out.println("The Result is: " + result);
+
+    }
+
+}
