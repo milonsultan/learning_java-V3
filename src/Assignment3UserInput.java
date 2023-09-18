bd60f074d15dcd9f9e790f9a0aa544aa6df8ad63
milonsultan <milon_sultan@hotmail.com>
2023-09-18 23:38:21 +0200

    Comment

--- src/Assignment3UserInput.java
+++ src/Assignment3UserInput.java
@@ -1,6 +1,13 @@
 public class Assignment3UserInput {
     public static void main(String[] args) {
 
+        int productId = 101;
+        String productTitle = "Iphone 15";
+        String productPrice = "1895 Euro";
+        String productDescription = "Perfect product with best image quality";
+        String productCategory = "Phone";
+
+
 
     }
 }
