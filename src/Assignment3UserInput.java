167b17440b920ba022e1dee36406e20ba021db94
milonsultan <milon_sultan@hotmail.com>
2023-09-18 23:52:56 +0200

    Comment

--- src/Assignment3UserInput.java
+++ src/Assignment3UserInput.java
@@ -1,13 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Assignment3UserInput {
+
     public static void main(String[] args) {
+        int id;
+        int price;
+        String title;
+        String description;
+        String category;
 
-        int productId = 101;
-        String productTitle = "Iphone 15";
-        String productPrice = "1895 Euro";
-        String productDescription = "Perfect product with best image quality";
-        String productCategory = "Phone";
+        System.out.println("Enter the product id:" );
+        id = input.nextInt();
+        System.out.println("The product title:" );
 
 
+        
 
     }
+
+
+
+
+
 }
