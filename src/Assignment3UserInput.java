39eba62244cdbf4d13c5fa902eca04e325acc073
milonsultan <milon_sultan@hotmail.com>
2023-09-19 00:41:38 +0200

    Assignment 3 User Input Done

--- src/Assignment3UserInput.java
+++ src/Assignment3UserInput.java
@@ -10,17 +10,29 @@ public class Assignment3UserInput {
         String description;
         String category;
 
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Enter the product id:" );
         id = input.nextInt();
-        System.out.println("The product title:" );
+        System.out.println("The product id:" +id);
 
+        System.out.println("Enter the product price:");
+        price = input.nextInt();
+        System.out.println("The product price:" +price);
 
-        
+        System.out.println("Enter the product title:");
+        title = input.next();
+        System.out.println("The product title:" +title);
+
+        System.out.println("Enter the product description:");
+        description = input.next();
+        System.out.println("The product description:" +description);
+
+        System.out.println("Enter the product category:");
+        category = input.next();
+        System.out.println("The product category:" +category);
+
 
     }
-
-
-
-
 
 }
