90f6bc6ff62dd59a9b1256cfeaa30a018068ed67
milonsultan <milon_sultan@hotmail.com>
2023-10-06 00:31:03 +0200

    Comment

--- src/Assignment4ArithmeticOperators.java
+++ src/Assignment4ArithmeticOperators.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Assignment4ArithmeticOperators {
 
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int phoneprice =1800; //1800 euro
+        int numberOfInstallments, installmentPerMonth;
+        System.out.print("Number of instalments? ");
+        numberOfInstallments = input.nextInt();
+        System.out.println("Installment per month? ");
+        installmentPerMonth = input.nextInt();
+
+
+
+
+
 
     }
 
